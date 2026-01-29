28b0dac359562de37ac6938bc532894f96a5e41a
jockejkpg <jockejkpg@gmail.com>
2026-01-29 22:36:56 +0100

    Add files via upload

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,5 @@
 export const SUPABASE_URL = "https://jqstbegfwerztttsmgqy.supabase.co";
-export const SUPABASE_ANON_KEY = "PASTA_IN_DIN_ANON_PUBLISHABLE_KEY_HAR";
+export const SUPABASE_ANON_KEY = "sb_publishable_oLbCALZI-QlTx-BAMl0BNw_PCYt6Xc9";
 
 export function createSupabaseClient() {
   if (!window.supabase) throw new Error("Supabase SDK saknas.");
